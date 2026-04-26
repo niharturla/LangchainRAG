ecbdb14abbcfee823841ef9a942f52120a67dce0
Nihar Turlapati <nturlapa@purdue.edu>
2026-04-26 17:37:53 -0400

    Added basic testing and functionality for agent

---
+++ main.py
@@ -0,0 +1,17 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model  = OllamaLLM(model="llama3.2")
+
+template = """
+You are an expert in answering questions about a pizza restaurant.
+
+Here are some relevant reviews: {reviews}
+Here is the question to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+res = chain.invoke({"reviews": [], "question": "What is the best pizza place in town?"})
+print(res)
